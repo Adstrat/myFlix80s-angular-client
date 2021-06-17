38a1e9e1070053674707ffb9a89335bb1434cf8e
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-06-17 22:13:43 +0200

    added profile view for when clicked from navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { SynopsisViewComponent } from './synopsis-view/synopsis-view.component';
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfileViewComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  GetUserService,
+} from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
+  user: any = {};
 
-  constructor() { }
+  constructor(
+    public fetchApiData: GetUserService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getUserInfo();
   }
+
+  /**
+   * Gets the user object from the database
+   */
+  getUserInfo(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.user = resp;
+
+    });
+  }
+
 
 }
